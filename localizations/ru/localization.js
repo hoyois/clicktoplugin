b3c968f7ffa1e32dfb18b877081a1ab6b50ac3a9
Marc Hoyois <marc.hoyois@gmail.com>
2010-09-11 17:27:01 -0500

    Russian localization

---
+++ localizations/ru/localization.js
@@ -0,0 +1,29 @@
+// ClickToFlash/ClickToPlugin localization file
+// Save with encoding: UTF-8
+
+// RUSSIAN
+
+const PLUGIN_GENERIC = "Дополнение";
+
+// Contextual menu items
+const LOAD_ALL_PLUGINS = "Подгрузить все Дополнения";
+const LOAD_ALL_FLASH = "Подгрузить Flash";
+const ADD_TO_LOC_WHITELIST = "Поместить адрес в Белый Список";
+const ADD_TO_SRC_WHITELIST = "Поместить источник в Белый Список";
+const VIEW_IN_QUICKTIME_PLAYER = "Смотреть в проигрывателе QuickTime";
+const LOAD_PLUGIN = function(plugin) {return "Подгрузить " + plugin;};
+const REMOVE_PLUGIN = function(plugin) {return "Скрыть " + plugin;};
+const RELOAD_IN_PLUGIN = function(plugin) {return "Показать в " + plugin;};
+const VIEW_ON_SITE = function(site) {return "Посмотреть на " + site;};
+
+// Dialogs
+const QT_CONFIRM_LAUNCH_DIALOG = function(url) {return "Объект QuickTime предлагает показать\n\n" + url + "\n\nв проигрывателе QuickTime. Позволить это сделать?";};
+const ADD_TO_LOC_WHITELIST_DIALOG = "Показывать дополнения встроенные в страницы с адресом вида:";
+const ADD_TO_SRC_WHITELIST_DIALOG = "Показывать дополнения с источников с адресом вида:";
+const ADD_TO_LOC_WHITELIST_DIALOG_FLASH = "Показывать Flash в страницах с адресом вида:";
+const ADD_TO_SRC_WHITELIST_DIALOG_FLASH = "Показывать Flash с источников с адресом вида:";
+
+// Injected scripts
+const VIDEO_LINK = "Скачать Видео";
+const AUDIO_LINK = "Скачать Звук";
+
