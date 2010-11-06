285dbaa1151c2727a4792f80c548964d1f7155c6
Marc Hoyois <marc.hoyois@gmail.com>
2010-11-06 12:13:41 -0500

    typo; Spanish localization

---
+++ localizations/es/localization.js
@@ -0,0 +1,30 @@
+// ClickToFlash/ClickToPlugin localization file
+// Save with encoding: UTF-8
+
+// ESPAÑOL
+
+const PLUGIN_GENERIC = "Complemento";
+
+// Contextual menu items
+const LOAD_ALL_PLUGINS = "Cargar todos los complementos";
+const LOAD_ALL_FLASH = "Cargar todo el Flash";
+const LOAD_INVISIBLE_PLUGINS = "Cargar los complementos invisibles";
+const LOAD_INVISIBLE_FLASH = "Cargar el Flash invisible";
+const ADD_TO_LOC_WHITELIST = "Añadir la dirección a la lista blanca";
+const ADD_TO_SRC_WHITELIST = "Añadir el proveedor a la lista blanca";
+const DOWNLOAD_VIDEO = "Descargar el vídeo";
+const DOWNLOAD_AUDIO = "Descargar el audio";
+const VIEW_IN_QUICKTIME_PLAYER = "Ver en QuickTime Player";
+const SHOW_ELEMENT = "Mostrar el elemento";
+const LOAD_PLUGIN = function(plugin) {return "Cargar " + plugin;};
+const REMOVE_PLUGIN = function(plugin) {return "Ocultar " + plugin;};
+const RELOAD_IN_PLUGIN = function(plugin) {return "Recargar en " + plugin;};
+const VIEW_ON_SITE = function(site) {return "Ver en " + site;};
+
+// Dialogs
+const QT_CONFIRM_LAUNCH_DIALOG = function(url) {return "Un objeto QuickTime quiere reproducirse\n\n" + url + "\n\nen QuickTime Player. Quieres reproducirlo?";};
+const ADD_TO_LOC_WHITELIST_DIALOG = "Permitir la carga de complementos en estas direcciones:";
+const ADD_TO_SRC_WHITELIST_DIALOG = "Permitir la carga de complementos que procedan de";
+const ADD_TO_LOC_WHITELIST_DIALOG_FLASH = "Permitir Flash en estas direcciones:";
+const ADD_TO_SRC_WHITELIST_DIALOG_FLASH = "Permitir Flash que proceda de";
+
