1fb381fe0ed2de05e895a5971a096f1fd7cbf809
Paul Grave <paul@stomer.net>
2012-01-03 20:52:34 +0000

    improved: bbc killer now retrieves the externalidentifier from an XHR instead of from saved state.

--- ClickToPlugin.safariextension/killers/BBC.js
+++ ClickToPlugin.safariextension/killers/BBC.js
@@ -19,66 +19,81 @@ addKiller("BBC", {
 "canKill": function(data) {
 	return data.params && data.params.id && data.location.indexOf("bbc.co.uk") !== -1;
 },
-"externalidentifiers": {},
 "process": function(data, callback) {
-	var identifier = data.params.id;
-	if( identifier && !this.externalidentifiers[identifier] && data.params.externalidentifier ){
-		this.externalidentifiers[identifier] = data.params.externalidentifier;
-	}
-
-	var flashvars = parseFlashVariables(data.params.flashvars);
-	var videoInfo = this.externalidentifiers[identifier];
-
-	if(!/emp.swf/.test(data.src)) {
+	
+	if(!/emp.swf/.test(data.src))
 		return;
-	}
-
+	
 	var flashvars = parseFlashVariables(data.params.flashvars);
-	var url = "http://open.live.bbc.co.uk/mediaselector/4/jsfunc/stream/" + videoInfo + "/processJSON/";
-
-	var processJSON = this.processJSON;
-	var posterURL = decodeURIComponent(flashvars.holdingImage);
-	var title = data.title;
-
+	var playlistURL = decodeURIComponent(flashvars.playlist);
+	
 	var xhr = new XMLHttpRequest();
-	xhr.open('GET', url, true);
-	xhr.onload = function() {
-
-		var processJSON = function(data) {
-			if( data.result != 'ok' )
-				return;
-
-			var siteInfo, sources;
-
-			sources = [];
-
-			data.media.forEach( function( media, i ) {
-				if( !(media.connection instanceof Array) && typeof(media['connection']) != undefined )
-					return;
-
-				var connection = media.connection[0];
-
-				if( typeof(connection['protocol']) != undefined && connection.protocol != 'http' )
-					return;
-
-				if( connection.href && media.bitrate && media.height )
-				   sources.push({"url": connection.href , "format":  media.bitrate + "k MP4", "height": media.height, "isNative": true});
-			});
-
-			callback({
-				"loadAfter": false,
-				"playlist": [{
-					"title": title,
-					"poster": posterURL,
-					"sources": sources
-			 }]});
-
-
+	xhr.open('GET', playlistURL, true);
+	xhr.onload = function (event) {
+		var doc = event.target.responseXML;
+		var mediatorElements = doc.getElementsByTagName('mediator');
+	
+		if( !mediatorElements.length ) 
+			return;
+		
+		var titleElements = doc.getElementsByTagName('title');
+		var title, poster;
+		
+		if ( titleElements.length ) {
+			title = titleElements[0].textContent;
 		};
+		
+		var linkElements = doc.getElementsByTagName('link');
+		
+		for( var i = 0; i < linkElements.length ; i++ ) {
+			if( linkElements[i].getAttribute('rel') == 'holding' ) {
+				poster = linkElements[i].getAttribute('href');
+				break;
+			}
+		}
+		
+		var url = "http://open.live.bbc.co.uk/mediaselector/4/jsfunc/stream/" + mediatorElements[0].getAttribute('identifier') + "/processJSON/";
+		
+		var xhr = new XMLHttpRequest();
+		xhr.open('GET', url, true);
+		xhr.onload = function(event) {
 
-		eval(xhr.responseText);
-	};
-	xhr.send(null);
+			var processJSON = function(data) {
+				if( data.result != 'ok' )
+					return;
+
+				var sources;
+
+				sources = [];
+
+				data.media.forEach( function( media, i ) {
+					if( !(media.connection instanceof Array) && typeof(media['connection']) != undefined )
+						return;
+
+					var connection = media.connection[0];
+
+					if( typeof(connection['protocol']) != undefined && connection.protocol != 'http' )
+						return;
+
+					if( connection.href && media.bitrate && media.height )
+					   sources.push({"url": connection.href , "format":  media.bitrate + "k MP4", "height": media.height, "isNative": true});
+				});
+
+				callback({
+					"playlist": [{
+						"title": title,
+						"poster": poster,
+						"sources": sources
+				 }]});
+			};
+			eval(event.target.response);
+		};
+		xhr.send();
+	}
+	xhr.send();
+	
+	
+
 }
 
 });
