3ce5e8c8267f33e1c11bfb69812c1a2fc9c4526a
Marc Hoyois <marchoyois@Marcs-MacBook-Pro.local>
2012-08-05 16:52:11 +0200

    minor Flash.js & Youtube.js updates

--- ClickToPlugin.safariextension/killers/YouTube.js
+++ ClickToPlugin.safariextension/killers/YouTube.js
@@ -79,6 +79,7 @@ addKiller("YouTube", {
 	*/
 	for(var i = 0; i < formatList.length; i++) {
 		var x = parseFlashVariables(formatList[i]);
+		if(i === 0 && !/signature%3D/.test(x.url)) return;
 		var videoURL = decodeURIComponent(x.url) + "&title=" + encodeURIComponent(flashvars.title);
 		if(x.itag === "38") {
 			sources.push({"url": videoURL, "format": "4K MP4", "height": 2304, "isNative": true});
