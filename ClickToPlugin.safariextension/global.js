ecf262d7d86df7c6d28b2f18fb51f117cb40f796
Marc Hoyois <marchoyois@Marcs-MacBook-Pro.local>
2012-09-16 16:05:41 -0500

    cleanup settings update up to 2.7

--- ClickToPlugin.safariextension/global.js
+++ ClickToPlugin.safariextension/global.js
@@ -3,55 +3,7 @@
 if(settings.version === undefined) {
 	openTab(safari.extension.baseURI + "settings.html");
 }
-if(settings.version < 41) {
-	var tmpArray = [];
-	for(var i = 0; i < settings.killers.length; i++) {
-		if(settings.killers[i] !== "killers/Vimeo.js") tmpArray.push(settings.killers[i]);
-	}
-	settings.killers = tmpArray;
-	
-	var shortcutList = ["addToWhitelist", "showAll", "hideAll", "hidePlugin", "playPause", "enterFullscreen", "volumeUp", "volumeDown", "prevTrack", "nextTrack", "trackSelector", "toggleLooping"];
-	var tmpKeys = settings.keys;
-	var tmpGestures = settings.gestures;
-	for(var i = 0; i < shortcutList.length; i++) {
-		var tmpId = shortcutList[i] + "Shortcut";
-		if(settings[tmpId]) {
-			if(settings[tmpId].type === "keydown") tmpKeys[shortcutList[i]] = settings[tmpId];
-			else tmpGestures[shortcutList[i]] = settings[tmpId];
-		}
-		settings.removeItem(tmpId);
-	}
-	if(settings.settingsShortcut) tmpKeys.prefPane = settings.settingsShortcut;
-	settings.removeItem("settingsShortcut");
-	settings.keys = tmpKeys;
-	settings.gestures = tmpGestures;
-}
-if(settings.version < 44) {
-	var tmpArray = settings.killers;
-	tmpArray.unshift("killers/Vimeo.js");
-	settings.killers = tmpArray;
-}
-if(settings.version < 45) {
-	var tmpArray = [];
-	for(var i = 0; i < settings.killers.length; i++) {
-		switch(settings.killers[i]) {
-		case "killers/IGN.js":
-		case "killers/MTVNetworks.js":
-		case "killers/BBC.js":
-		case "killers/CollegeHumor.js":
-		case "killers/Tumblr.js":
-		case "killers/Flash.js":
-		case "killers/Silverlight.js":
-		case "killers/Generic.js":
-			break;
-		default:
-			tmpArray.push(settings.killers[i]);
-		}
-	}
-	tmpArray.push("killers/MTVNetworks.js", "killers/BBC.js", "killers/Brightcove.js", "killers/CollegeHumor.js", "killers/Tumblr.js", "killers/Flash.js", "killers/Silverlight.js", "killers/Generic.js");
-	settings.killers = tmpArray;
-}
-settings.version = 45;
+settings.version = 46;
 
 // LOCALIZATION
 localize(GLOBAL_STRINGS, settings.language);
